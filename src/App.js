8fa8a813257ce1e586cf3c8af7cdf26209af9007
Lucas <lecanete@gmail.com>
2022-08-18 18:41:12 -0300

    NavBAr

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './components/NavBar';
+
+function App() {
+  return (
+    <div className="App">
+
+      <NavBar/>
+
+      <div>
+      <h1>Tienda de Ropita carteluda</h1>
+      <h2>Ropa Exclusiva</h2>
+      </div>
+
+      <div>footer</div>
+    </div>
+  );
+}
+
+export default App;
